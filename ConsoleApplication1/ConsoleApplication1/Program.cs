de66e35155c9143914292b61c2530a68ed83b788
jubermulani <54530747+jubermulani@users.noreply.github.com>
2019-08-26 18:25:25 +0530

    comments added from git

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("hi this is the chagne ");
+            //added comment from git
         }
     }
 }
